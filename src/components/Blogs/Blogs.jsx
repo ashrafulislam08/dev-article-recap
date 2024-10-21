a024ad9179dbad10ec0322d33999b5caed4970a3
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-10-22 01:31:11 +0600

    Fetched blogs data displayed to the page

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Blog from "../Blog/Blog";
+
 export const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((response) => response.json())
+      .then((data) => setBlogs(data));
+  }, []);
+
   return (
     <div className="w-2/3">
-      <h4 className="text-4xl">Blogs</h4>
+      <h4 className="text-4xl">Blogs: {blogs.length}</h4>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
     </div>
   );
 };
